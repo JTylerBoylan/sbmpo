5a793387225456f75fc4da78b380be6167df6d21
Jonathan Tyler Boylan <63868524+JTylerBoylan@users.noreply.github.com>
2022-10-26 00:29:14 -0400

    Delete saveconfig.m

--- sbmpo_models/matlab/saveconfig.m
+++
@@ -1,7 +0,0 @@
-function saveconfig(max_iter, max_gen, samp_time, samp_del_time, goal_thres, init_state, goal_state, grid_act, init_control, grid_res, grid_size, branchout)
-%TOPLANNERCONFIG Convert params to config file
-
-    conf = [max_iter' max_gen' samp_time' samp_del_time' goal_thres' init_state' goal_state' grid_act' init_control', grid_res', grid_size'];
-
-end
-
