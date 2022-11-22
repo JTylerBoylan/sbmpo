10c1e6070627164122763f835c10922ae2613d1e
jtylerboylan <jtylerboylan@outlook.com>
2022-11-22 00:35:17 -0500

    Benchmark 4 update

--- sbmpo_models/matlab/BookModelConfig_BM4.m
+++ sbmpo_models/matlab/BookModelConfig_BM4.m
@@ -7,7 +7,7 @@ clc
 %% Parameters
 
 % Set total number of runs
-runs = 41;
+runs = 79;
 
 MaxIterations = 30000;
 MaxGenerations = 300;
@@ -22,7 +22,7 @@ InitialState = [0, 0, 1.5707];
 GoalState = [5, 5, 0];
 InitialControl = [0, 0];
 GridActiveStates = [1, 1, 0];
-GridResolution = [0.0:0.0025:0.1; 0.0:0.0025:0.1]';
+GridResolution = [0.002:0.001:0.08; 0.002:0.001:0.08]';
 
 RotationControls = {
         [0 -0.785398 0.785398];
