7e252a2b39e4e6c4b0b529cd34bc11b7ab314f63
JTyler <jtylerboylan@outlook.com>
2023-05-16 21:09:00 -0400

    Added quit() function

--- docker/sbmpo_docker.sh
+++ docker/sbmpo_docker.sh
@@ -7,4 +7,4 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 docker build -t sbmpo:latest -f "${SCRIPT_DIR}/Dockerfile.sbmpo" "${SCRIPT_DIR}"
 
 # Start the Docker container with the current directory mounted to /app, and automatically remove the container when it is stopped or exited
-docker run --rm -it --mount type=bind,source="${SCRIPT_DIR}",target=/sbmpo_ws/sbmpo/ sbmpo:latest
+docker run --rm -it --mount type=bind,source="${SCRIPT_DIR}/../",target=/sbmpo_ws/ sbmpo:latest
