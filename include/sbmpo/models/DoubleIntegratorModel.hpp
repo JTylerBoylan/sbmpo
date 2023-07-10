7e37489953a9d8f40d708253d14fdd5ab6e07555
Jonathan T. Boylan <jtylerboylan@outlook.com>
2023-07-09 21:32:02 -0400

    de-clutter src

--- include/sbmpo/models/DoubleIntegratorModel.hpp
+++ include/sbmpo/models/DoubleIntegratorModel.hpp
@@ -23,15 +23,50 @@ class DoubleIntegratorModel : public Model {
         integration_steps_ = 10;
     }
 
-    virtual State next_state(const State &state, const Control& control, const float time_span);
+    virtual State next_state(const State &state, const Control& control, const float time_span) {
+        // Update state
+        State next_state = state;
+        float time_step = time_span / integration_steps_;
+        for (int i = 0; i < integration_steps_; i++) {
+            next_state[X] += state[V] * time_step;
+            next_state[V] += control[A] * time_step;
+        }
+        return next_state;
+    }
 
-    virtual float cost(const State& state, const Control& control, const float time_span);
+    virtual float cost(const State& state, const Control& control, const float time_span) {
+        return time_span;
+    }
 
-    virtual float heuristic(const State& state, const State& goal);
+    virtual float heuristic(const State& state, const State& goal) {
 
-    virtual bool is_valid(const State& state);
+        /*
+        *   Time Optimal Heuristic
+        */
 
-    virtual bool is_goal(const State& state, const State& goal);
+        float q_10 = state[X] - goal[X];
+        float q_20 = state[V] - goal[V];
+
+        float b = 0, c = 0;
+        if (q_10 + 0.5f*q_20*std::abs(q_20) / max_acc_ >= 0) {
+            b = 2.0f * q_20 / min_acc_;
+            c = (q_20*q_20 + 2.0f*(max_acc_ - min_acc_)*q_10) / (max_acc_ * min_acc_);
+        } else if (q_10 - 0.5f*q_20*std::abs(q_20) / min_acc_ < 0) {
+            b = 2.0f * q_20 / max_acc_;
+            c = (q_20*q_20 - 2.0f*(max_acc_ - min_acc_)*q_10) / (max_acc_ * min_acc_);
+        }
+
+        return 0.5f * (-b + sqrtf(b*b - 4*c));
+    }
+
+    virtual bool is_valid(const State& state) {
+        return true;
+    }
+
+    virtual bool is_goal(const State& state, const State& goal) {
+        return std::abs(goal[X] - state[X]) <= threshold_x_
+            && std::abs(goal[V] - state[V]) <= threshold_v_;
+    }
 
     void set_integration_steps(size_t integration_steps) {
         integration_steps_ = integration_steps;
